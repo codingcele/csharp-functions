d63ea0bccad96dcd035aca67670d8a19427eedad
codingcele <marcocelegato9@gmail.com>
2023-04-06 16:02:45 +0200

    snack 1

--- Program.cs
+++ Program.cs
@@ -1,2 +1,28 @@
 ﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+
+using System;
+
+namespace Snacks
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] numeri = { 1, 2, 3, 4, 5, 12, 17, 11, 28, 10 };
+            StampaArray(numeri);
+        }
+
+        static void StampaArray(int[] array)
+        {
+            Console.Write("[");
+            for (int i = 0; i < array.Length; i++)
+            {
+                Console.Write(array[i]);
+                if (i != array.Length - 1)
+                    Console.Write(", ");
+            }
+            Console.Write("]");
+        }
+    }
+}
+
